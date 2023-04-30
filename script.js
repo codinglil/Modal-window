3538db1381d12b235222c0d7b93dd1d93725ee17
codinglil <102619991+codinglil@users.noreply.github.com>
2023-04-30 06:50:30 -0400

    js
    added event listener on click with loop

--- script.js
+++ script.js
@@ -5,4 +5,13 @@ let overlay = document.querySelector(".overlay");
 let btnClose = document.querySelector(".close-modal");
 let btnsOpenModals = document.querySelectorAll(".show-modal");
 
-for(let i = 0; i < btnsOpenModals; i++)
+for (let i = 0; i < btnsOpenModals.length; i++) {
+  btnsOpenModals[i].addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+  btnClose.addEventListener("click", function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  });
+}
