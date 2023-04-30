469033a150e503dc4b8c9417b602f8b54d01c8d1
codinglil <102619991+codinglil@users.noreply.github.com>
2023-04-30 05:52:09 -0400

    html, css
    js variables

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let modal = document.querySelector(".modal");
+let overlay = document.querySelector(".overlay");
+let btnClose = document.querySelector(".close-modal");
+let btnsOpenModals = document.querySelectorAll(".show-modal");
+
+for(let i = 0; i < btnsOpenModals; i++)
